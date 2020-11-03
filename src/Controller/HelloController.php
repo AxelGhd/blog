eabc1e6040aa4485ae2cceb2a4fbab26e1969a2e
Axel Guihard <axelg-44@hotmail.fr>
2020-11-03 10:01:51 +0100

    routes with parameters and add

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -7,10 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 class HelloController extends AbstractController {
 
     public function demo1(){
-        return new Response('Yes, ça marche !');
+        return new Response('Let\'s live a dream together Matéo, we can be happy and have a great life together, i\'ll make you happy i promise !');
     }
 
-    public function demo2($name){
+    public function demo2(string $name){
         return new Response('Bonjour '.$name.' !');
     }
 
